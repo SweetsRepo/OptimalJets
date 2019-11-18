e6137bde314aca0028ed34ceaa83b5775d9b50a6
Chris Sweet <chris.sweet0320@gmail.com>
2019-11-17 21:06:20 -0500

    Successfully merged sensor reading code into Main Acticity

--- app/src/main/java/com/example/optimaljets/MainActivity.java
+++ app/src/main/java/com/example/optimaljets/MainActivity.java
@@ -4,11 +4,21 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
 import android.view.Menu;
 import android.view.MenuItem;
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
+    private SensorManager sensorManager;
+    private Sensor pressure;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Assign instances of the sensor manager and pressure sensor on the device
+        sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        pressure = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
     }
 
     @Override
@@ -38,5 +52,33 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public final void onAccuracyChanged(Sensor sensor, int accuracy){
+        // Just needs to be implemented to make SensorEventListener Interface happy
+    }
+
+    @Override
+    public final void onSensorChanged(SensorEvent event){
+        if(event.sensor.getType() != Sensor.TYPE_PRESSURE)
+            return;
+        float millibarsOfPressure = event.values[0];
+        // TODO: Convert mBar to hPa
+        // TODO: Collect Temperature in K or C as well as RH
+        TextView jetSize = (TextView)findViewById(R.id.jetSize);
+        jetSize.setText(String.valueOf(millibarsOfPressure));
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        sensorManager.registerListener(this, pressure, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        sensorManager.unregisterListener(this);
     }
 }
